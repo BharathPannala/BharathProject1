13098b242702f1a0c491c00e66a91f4e82ae04b2
Danal <Danal@192.168.1.105>
2017-04-30 23:11:56 +0530

    Updated Sample java file

--- TestProjectSample/src/com/nt/test/Sample.java
+++ TestProjectSample/src/com/nt/test/Sample.java
@@ -5,7 +5,7 @@ public class Sample {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Sample Code By TL");
+		System.out.println("Sample Code By TL hello");
 	}
 
 }
