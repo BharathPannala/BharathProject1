c274011358cbfd3242ec3d9b478598b29826f883
Danal <Danal@192.168.1.105>
2017-04-30 21:57:30 +0530

    Adding SampleApp to Project

---
+++ TestProj1/src/com/nt/test/SampleApp.java
@@ -0,0 +1,12 @@
+package com.nt.test;
+
+public class SampleApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("TL Change to Project ");
+
+	}
+
+}
